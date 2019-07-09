0659b1dd73381f24b7381d82f560157480f39a3e
theHarshit1 <harshit@github>
2019-07-09 16:37:21 +0530

    pro2

--- pro2.py
+++ pro2.py
@@ -1,12 +1,13 @@
 n,k=input().strip().split(" ")
 k=int(k)
 i=0;
-while i<len(n)-1 and k:
+size=len(n)
+while i<size-1 and k:
 	if(n[i]>n[i+1]):
+		k-=1
 		n=n.replace(n[i],"")
 		if(i!=0):
 			i-=1
-		k-=1
 	else:
 		i+=1
 res=n[:len(n)-k]
