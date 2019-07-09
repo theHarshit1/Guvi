02a6cfd5713767267cc8cd12ae52e16dd7e2a025
theHarshit1 <harshit@github>
2019-07-10 02:33:57 +0530

    pro13

---
+++ pro13.py
@@ -0,0 +1,8 @@
+n,q=list(map(int,input().split()))
+l=list(map(int,input().split()))
+queries=[]
+while(q):
+	queries.append(list(map(int,input().split())))
+	q-=1
+for i in queries:
+	print(sum(l[i[0]-1:i[1]]))
