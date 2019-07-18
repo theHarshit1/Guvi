c4606244a150696add5c5e6207fe66ff05993764
theHarshit1 <harshit@github>
2019-07-18 20:43:57 +0530

    pro31

--- pro31.py
+++ pro31.py
@@ -1,15 +1,16 @@
-
-def maxsum(l):
-	for i in l:
-		
+def maxsum(l,count):
+	if(len(l)==0):
+		return count
+	x=maxsum(g[l[0]-1],count+l[0])
+	y=maxsum(l[1:],count)
+	return max(x,y)
 
 n,m=map(int,input().split())
 g=[]
+for _ in range(n):
+	g.append([])
 for _ in range(m):
 	a,b=list(map(int,input().split()))
-	if a<b:
-		g[a].append(b)
-	else:
-		g[b].append(a)
-maxsum(g[0])
-
+	g[a-1].append(b)
+s=maxsum(g[0],1)
+print(s)
