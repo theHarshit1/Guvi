e023f85f2a22871a724e2bb1d49e1b220b147157
theHarshit1 <harshit@github>
2019-07-19 20:42:08 +0530

    pro24

--- pro24.py
+++ pro24.py
@@ -1,4 +1,11 @@
 n=int(input())
 p=2**n
-for i in range(p):
-	print(format(i,'0'+str(n)+'b')) 
+l=[]
+for i in range(n):
+	l.append([])
+for j in range(1,p):
+	s=format(j,'0'+str(n)+'b')
+	l[s.count('1')-1].append(s)
+print('0'*n)
+for i in l:
+	print(*i,sep='\n')
