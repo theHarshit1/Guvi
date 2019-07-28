9937c5a9b6f13d3052d512b488e36bc364cc40a0
theHarshit1 <harshit@github>
2019-07-28 20:32:00 +0530

    pro43

--- pro43.py
+++ pro43.py
@@ -1,10 +1,19 @@
 n=int(input())
 l=list(map(int,input().split()))
-j=1
-for i in range(len(l)):
-	if l[i] in l[i+1:]:
-		while(j in l):
-			j+=1
-		l[i]=j
-print(j)
-print(*l,sep=' ')
+ntimes=[]
+visited=[]
+c=0
+for i in range(n):
+	ntimes.append(l.count(i+1))
+for i in range(n):
+	if(ntimes[l[i]-1]>1):
+		ind=ntimes.index(0)+1
+		if(l[i]>ind or l[i] in visited):
+			ntimes[l[i]-1]-=1
+			l[i]=ind
+			ntimes[l[i]-1]=1
+			c+=1
+		else:
+			visited.append(l[i])
+print(c)
+print(*l)
